c8b781331535c62974921ef470f596c0a92db6e6
ThatXliner <bryan.hu.2020@gmail.com>
2025-01-06 16:20:54 -0800

    :sparkles: Generic templates

---
+++ src/main/java/frc/robot/utils/generics/PositionIO.java
@@ -0,0 +1,37 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.utils.generics;
+
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public interface PositionIO {
+
+  public default void updateInputs(SingleMotorSubsystemInputs inputs) {}
+
+  public default void setVoltage(double voltage) {}
+
+  public default void setPosition(double position) {}
+
+  public default TalonFX getMotor() {
+    return new TalonFX(0);
+  }
+
+  public default VoltageOut getVoltageRequest() {
+    return new VoltageOut(0);
+  }
+
+  public default void off() {
+    this.getMotor().setControl(new NeutralOut());
+  }
+
+  public default void zero() {
+    this.getMotor().setPosition(0);
+  }
+}
