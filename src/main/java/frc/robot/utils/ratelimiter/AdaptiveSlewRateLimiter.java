2531fa338f2a9e67b213134d9d04614b32808f25
Josh <102478723+realjoshuau@users.noreply.github.com>
2025-01-09 08:28:34 -0800

    spotless

--- src/main/java/frc/robot/utils/ratelimiter/AdaptiveSlewRateLimiter.java
+++ src/main/java/frc/robot/utils/ratelimiter/AdaptiveSlewRateLimiter.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 FRC 3256
 // https://github.com/Team3256
 //
-// Use of this source code is governed by an MIT-style
+// Use of this source code is governed by a 
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
@@ -11,40 +11,41 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.util.WPIUtilJNI;
 
 public class AdaptiveSlewRateLimiter {
-    private final double accelRateLimit;
-    private final double decelRateLimit;
-    private double prevVal;
-    private double prevTime;
+  private final double accelRateLimit;
+  private final double decelRateLimit;
+  private double prevVal;
+  private double prevTime;
 
-    public AdaptiveSlewRateLimiter(double accelRateLimit, double decelRateLimit) {
-        this.accelRateLimit = Math.abs(accelRateLimit);
-        this.decelRateLimit = Math.abs(decelRateLimit);
+  public AdaptiveSlewRateLimiter(double accelRateLimit, double decelRateLimit) {
+    this.accelRateLimit = Math.abs(accelRateLimit);
+    this.decelRateLimit = Math.abs(decelRateLimit);
 
-        prevVal = 0;
-        prevTime = WPIUtilJNI.now() * 1e-6;
-    }
+    prevVal = 0;
+    prevTime = WPIUtilJNI.now() * 1e-6;
+  }
 
-    /**
-     * Filters the input to limit its slew rate.
-     *
-     * @param input
-     * @return
-     */
-    public double calculate(double input) {
-        double currentTime = WPIUtilJNI.now() * 1e-6;
-        double elapsedTime = currentTime - this.prevTime;
+  /**
+   * Filters the input to limit its slew rate.
+   *
+   * @param input
+   * @return
+   */
+  public double calculate(double input) {
+    double currentTime = WPIUtilJNI.now() * 1e-6;
+    double elapsedTime = currentTime - this.prevTime;
 
-        double currRateLimit = (Math.abs(input) > Math.abs(prevVal) ? accelRateLimit : decelRateLimit);
+    double currRateLimit = (Math.abs(input) > Math.abs(prevVal) ? accelRateLimit : decelRateLimit);
 
-        prevVal += MathUtil.clamp(input - prevVal, -currRateLimit * elapsedTime, currRateLimit * elapsedTime);
+    prevVal +=
+        MathUtil.clamp(input - prevVal, -currRateLimit * elapsedTime, currRateLimit * elapsedTime);
 
-        prevTime = currentTime;
+    prevTime = currentTime;
 
-        return prevVal;
-    }
+    return prevVal;
+  }
 
-    public void reset(double value) {
-        prevVal = value;
-        prevTime = WPIUtilJNI.now() * 1e-6;
-    }
+  public void reset(double value) {
+    prevVal = value;
+    prevTime = WPIUtilJNI.now() * 1e-6;
+  }
 }
