3e68f45358b58dec9de88fcba9a8af1b32cce2b9
Josh <102478723+realjoshuau@users.noreply.github.com>
2025-01-09 08:28:19 -0800

    swerve acceleration limiting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,11 +10,15 @@ package frc.robot;
 import edu.wpi.first.epilogue.Logged;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -52,6 +56,7 @@ public final class Constants {
 
     public static final boolean kSwerveEnabled = true;
     public static final boolean kVisionEnabled = false;
+    public static final boolean kSwerveAccelerationLimitingEnabled = true;
   }
 
   public static class Logging {
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,38 +7,52 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
 import choreo.auto.AutoChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.FeatureFlags;
 import frc.robot.commands.AutoRoutines;
 import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
 import frc.robot.subsystems.swerve.generated.TunerConstants;
 import frc.robot.utils.MappedXboxController;
+import frc.robot.utils.ratelimiter.AdaptiveSlewRateLimiter;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  public final MappedXboxController m_driverController =
-      new MappedXboxController(ControllerConstants.kDriverControllerPort, "driver");
-  public final MappedXboxController m_operatorController =
-      new MappedXboxController(ControllerConstants.kOperatorControllerPort, "operator");
+  public final MappedXboxController m_driverController = new MappedXboxController(
+      ControllerConstants.kDriverControllerPort, "driver");
+  public final MappedXboxController m_operatorController = new MappedXboxController(
+      ControllerConstants.kOperatorControllerPort, "operator");
 
   private final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+  /* Swerve Rate Limiting */
+  private final AdaptiveSlewRateLimiter swerveVelXRateLimiter = new AdaptiveSlewRateLimiter(0.5, 0.5);
+  private final AdaptiveSlewRateLimiter swerveVelYRateLimiter = new AdaptiveSlewRateLimiter(0.5, 0.5);
+  private final AdaptiveSlewRateLimiter swerveAngVelRateLimiter = new AdaptiveSlewRateLimiter(0.5, 0.5);
 
   private final AutoRoutines m_autoRoutines;
   private final AutoChooser autoChooser = new AutoChooser();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
@@ -47,19 +61,25 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
-    //    m_driverController.b("Example
+    // m_driverController.b("Example
     // method").whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
@@ -73,5 +93,31 @@ public class RobotContainer {
 
     // Schedule the selected auto during the autonomous period
     RobotModeTriggers.autonomous().whileTrue(autoChooser.selectedCommandScheduler());
+  }
+
+  private void configureSwerve() {
+    // LinearVelocity is a vector, so we need to get the magnitude
+    double MaxSpeed = TunerConstants.kSpeedAt12Volts.magnitude();
+    double MaxAngularRate = 1.5 * Math.PI; // My drivetrain
+    double SlowMaxSpeed = MaxSpeed * 0.3;
+    double SlowMaxAngular = MaxAngularRate * 0.3;
+
+    SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric();
+    if (FeatureFlags.kSwerveAccelerationLimitingEnabled) {
+      drivetrain.applyRequest(
+          () -> drive
+              .withVelocityX(swerveVelXRateLimiter.calculate(m_driverController.getLeftY() * MaxSpeed)) // Drive -y is
+                                                                                                        // forward
+              .withVelocityY(swerveVelYRateLimiter.calculate(m_driverController.getLeftX() * MaxSpeed)) // Drive -x is
+                                                                                                        // left
+              .withRotationalRate(swerveAngVelRateLimiter.calculate(-m_driverController.getRightX() * MaxAngularRate)));
+    } else {
+      drivetrain.applyRequest(
+          () -> drive
+              .withVelocityX(m_driverController.getLeftY() * MaxSpeed) // Drive -y is forward
+              .withVelocityY(m_driverController.getLeftX() * MaxSpeed) // Drive -x is left
+              .withRotationalRate(-m_driverController.getRightX() * MaxAngularRate));
+    }
+
   }
 }
