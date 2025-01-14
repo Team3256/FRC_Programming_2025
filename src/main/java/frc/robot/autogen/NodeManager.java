5e3d22372d084a559fce131a974442e226503e94
Ryan Abraham <ryan.ryanabraham@gmail.com>
2025-01-13 17:29:49 -0800

    add levels

--- src/main/java/frc/robot/autogen/NodeManager.java
+++ src/main/java/frc/robot/autogen/NodeManager.java
@@ -60,8 +60,24 @@ public class NodeManager {
               routine.trajectory(
                   node.intakeLocation().name() + "-" + node.scoringLocation().name());
           // Wait for intake traj to be done then trigger scoring traj
-          intakeTraj.done().toggleOnTrue(scoringTraj.cmd());
-          scoringTraj.done().toggleOnTrue(rollers.setRollerVoltage(-3));
+          switch (node.scoringType()) {
+            case L1 -> {
+              intakeTraj.done().toggleOnTrue(scoringTraj.cmd());
+              scoringTraj.done().toggleOnTrue(rollers.setRollerVoltage(-3));
+            }
+            case L2 -> {
+              intakeTraj.done().toggleOnTrue(scoringTraj.cmd());
+              scoringTraj.done().toggleOnTrue(rollers.setRollerVoltage(-3));
+            }
+            case L3 -> {
+              intakeTraj.done().toggleOnTrue(scoringTraj.cmd());
+              scoringTraj.done().toggleOnTrue(rollers.setRollerVoltage(-3));
+            }
+            case L4 -> {
+              intakeTraj.done().toggleOnTrue(scoringTraj.cmd());
+              scoringTraj.done().toggleOnTrue(rollers.setRollerVoltage(-3));
+            }
+          }
 
           // Update last scoring location and trigger for next traj
           lastScoringLocation = node.scoringLocation();
