94ccec5ebc040a2acd9abd902fa14fa1429b08b4
Grace Xu <71201761+gxu0904@users.noreply.github.com>
2025-01-17 15:14:17 -0800

    Spotless Apply

--- src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -1,12 +1,18 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.subsystems.climb;
 
 public class ClimbConstants {
-    public static final int kClimbMotorID = 0;
-    public static final double kMaxCurrent = 30;
-    public static final double kStowPos = 0;
-    public static final double kIntakePos = 90;
-    public static final double kLowVoltage = 0.1;
-    public static final double kDegreesPerMotorRotation = 360;
-    public static final double kMotorTicksPerRotation = 2048;
-
+  public static final int kClimbMotorID = 0;
+  public static final double kMaxCurrent = 30;
+  public static final double kStowPos = 0;
+  public static final double kIntakePos = 90;
+  public static final double kLowVoltage = 0.1;
+  public static final double kDegreesPerMotorRotation = 360;
+  public static final double kMotorTicksPerRotation = 2048;
 }
--- src/main/java/frc/robot/subsystems/climb/ClimbSubsytem.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbSubsytem.java
@@ -1,3 +1,10 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.subsystems.climb;
 
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
@@ -6,37 +13,38 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimbSubsytem extends SubsystemBase{
-    private final TalonFX climbMotor = new TalonFX(ClimbConstants.kClimbMotorID);
+public class ClimbSubsytem extends SubsystemBase {
+  private final TalonFX climbMotor = new TalonFX(ClimbConstants.kClimbMotorID);
 
-    private final MotionMagicVoltage motionMagic = new MotionMagicVoltage(ClimbConstants.kClimbMotorID);
+  private final MotionMagicVoltage motionMagic =
+      new MotionMagicVoltage(ClimbConstants.kClimbMotorID);
 
-    public ClimbSubsytem() {
-        climbMotor.getConfigurator().apply(new com.ctre.phoenix6.configs.TalonFXConfiguration());
-    }
+  public ClimbSubsytem() {
+    climbMotor.getConfigurator().apply(new com.ctre.phoenix6.configs.TalonFXConfiguration());
+  }
 
-    public void setDegrees(double deg) {
-        double targetPosition = deg / (ClimbConstants.kDegreesPerMotorRotation) * (ClimbConstants.kMotorTicksPerRotation);
-        climbMotor.setControl(new MotionMagicVoltage(targetPosition));
-    }
+  public void setDegrees(double deg) {
+    double targetPosition =
+        deg / (ClimbConstants.kDegreesPerMotorRotation) * (ClimbConstants.kMotorTicksPerRotation);
+    climbMotor.setControl(new MotionMagicVoltage(targetPosition));
+  }
 
-    public void zeroClimb() {
-        climbMotor.setControl(new MotionMagicVoltage(ClimbConstants.kLowVoltage));
-        while (!isCurrentSpiking()) {}
-        climbMotor.setControl(new MotionMagicVoltage(0));
-    }
+  public void zeroClimb() {
+    climbMotor.setControl(new MotionMagicVoltage(ClimbConstants.kLowVoltage));
+    while (!isCurrentSpiking()) {}
+    climbMotor.setControl(new MotionMagicVoltage(0));
+  }
 
+  public boolean isCurrentSpiking() {
+    return climbMotor.getSupplyCurrent().getValueAsDouble() > ClimbConstants.kMaxCurrent;
+  }
 
-    public boolean isCurrentSpiking() {
-        return climbMotor.getSupplyCurrent().getValueAsDouble() > ClimbConstants.kMaxCurrent;
-    }
-
-    public Command stowClimb() {
-        return new InstantCommand(
-            () -> {
-                zeroClimb();
-                setDegrees(ClimbConstants.kStowPos);
-            },
+  public Command stowClimb() {
+    return new InstantCommand(
+        () -> {
+          zeroClimb();
+          setDegrees(ClimbConstants.kStowPos);
+        },
         this);
-    }
+  }
 }
