92a81cb5157a96954bd78c6137a32fd0aada5b36
Jerry Fan <jerryfan5433@gmail.com>
2025-01-17 17:11:30 -0800

    Boiler Plate for Ground Intake Finished

---
+++ src/main/java/frc/robot/subsystems/groundIntake/GroundIntakeIOSim.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems.groundIntake;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.sim.CANcoderSimState;
+import com.ctre.phoenix6.sim.ChassisReference;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import frc.robot.sim.SimMechs;
+import org.littletonrobotics.junction.LoggedRobot;
+
+public class GroundIntakeIOSim extends GroundIntakeIOTalonFX {
+
+  private final SingleJointedArmSim groundIntakeSimModel =
+      new SingleJointedArmSim(
+          DCMotor.getKrakenX60(1),
+          GroundIntakeConstants.Sim.simGearing,
+          GroundIntakeConstants.Sim.jkGMetersSquared,
+          GroundIntakeConstants.Sim.armLength.in(Meters),
+          GroundIntakeConstants.Sim.minAngle.getRadians(),
+          GroundIntakeConstants.Sim.maxAngle.getRadians(),
+          true,
+          GroundIntakeConstants.Sim.startingAngle.getRadians()); //Change this model later to be Ground Intake
+
+
+  private TalonFXSimState groundIntakeSimState;
+  private CANcoderSimState cancoderSimState;
+
+  public GroundIntakeIOSim() {
+    super();
+    groundIntakeSimState = super.getMotor().getSimState();
+    cancoderSimState = super.getEncoder().getSimState();
+    cancoderSimState.Orientation = ChassisReference.Clockwise_Positive;
+    groundIntakeSimState.Orientation = ChassisReference.Clockwise_Positive;
+  }
+
+  @Override
+  public void updateInputs(GroundIntakeIOInputs inputs) {
+
+    groundIntakeSimState = super.getMotor().getSimState();
+    groundIntakeSimState.setSupplyVoltage(RobotController.getBatteryVoltage());
+    groundIntakeSimModel.setInputVoltage(groundIntakeSimState.getMotorVoltage());
+    groundIntakeSimModel.update(LoggedRobot.defaultPeriodSecs);
+    groundIntakeSimState.setRawRotorPosition(
+        Units.radiansToRotations(groundIntakeSimModel.getAngleRads()) * GroundIntakeConstants.Sim.simGearing);
+        groundIntakeSimState.setRotorVelocity(
+        Units.radiansToRotations(groundIntakeSimModel.getVelocityRadPerSec()) * GroundIntakeConstants.Sim.simGearing);
+    RoboRioSim.setVInVoltage(
+        BatterySim.calculateDefaultBatteryLoadedVoltage(groundIntakeSimModel.getCurrentDrawAmps()));
+
+    cancoderSimState = super.getEncoder().getSimState();
+    cancoderSimState.setSupplyVoltage(RobotController.getBatteryVoltage());
+    cancoderSimState.setRawPosition(Radians.of(groundIntakeSimModel.getAngleRads()).in(Rotations));
+    groundIntakeSimState.setRotorVelocity(
+        RadiansPerSecond.of(groundIntakeSimModel.getVelocityRadPerSec()).in(RotationsPerSecond));
+    super.updateInputs(inputs);
+    SimMechs.getInstance().updateArm(Radians.of(groundIntakeSimModel.getAngleRads()));
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/groundIntake/GroundIntakeIOTalonFX.java
@@ -0,0 +1,122 @@
+package frc.robot.subsystems.groundIntake;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.utils.PhoenixUtil;
+import frc.robot.subsystems.groundIntake.GroundIntakeConstants.Sim;
+
+
+public class GroundIntakeIOTalonFX implements GroundIntakeIO{
+    private final TalonFX motor;
+    private final PositionVoltage positionRequest = new PositionVoltage(0).withSlot(0);
+    private final MotionMagicVoltage motionMagicRequest = new MotionMagicVoltage(0).withSlot(0);
+    private final VoltageOut voltageReq = new VoltageOut(0);
+
+    private final StatusSignal<Voltage> motorVoltage;
+    private final StatusSignal<Angle> motorPosition;
+    private final StatusSignal<Current> motorStatorCurrent;
+    private final StatusSignal<Current> motorSupplyCurrent;
+    //private final StatusSignal<Temperature> motorTemperature;
+    private final StatusSignal<Double> motorReferenceSlope;
+    private final CANcoder cancoder = new CANcoder(GroundIntakeConstants.groundIntakeMotorEncoderId);
+
+    public GroundIntakeIOTalonFX() 
+    {
+        motor = new TalonFX(GroundIntakeConstants.kMotorID);
+        motorVoltage = motor.getMotorVoltage();
+        motorPosition = motor.getPosition();
+        motorStatorCurrent = motor.getStatorCurrent();
+        motorSupplyCurrent = motor.getSupplyCurrent();
+        //motorTemperature = motor.getDeviceTemp();
+        motorReferenceSlope = motor.getClosedLoopReferenceSlope();
+        
+   
+        PhoenixUtil.applyMotorConfigs(
+        motor, GroundIntakeConstants.kMotorConfig, GroundIntakeConstants.kFlashConfigRetries);
+        
+        BaseStatusSignal.setUpdateFrequencyForAll(
+            GroundIntakeConstants.kStatusSignalUpdateFrequency,
+            motorVoltage,
+            motorPosition,
+            motorStatorCurrent,
+            motorSupplyCurrent,
+            // motorTemperature,
+            motorReferenceSlope
+        );
+        motor.optimizeBusUtilization();
+    }
+            @Override
+        public void updateInputs(GroundIntakeIOInputs inputs) {
+            BaseStatusSignal.refreshAll(
+                motorVoltage, 
+                motorPosition,
+                motorStatorCurrent,
+                motorSupplyCurrent,
+                //motorTemperature,
+                motorReferenceSlope);
+
+            inputs.motorVoltage = motorVoltage.getValueAsDouble();
+            inputs.motorPosition = motorPosition.getValueAsDouble();
+            inputs.motorStatorCurrent = motorStatorCurrent.getValueAsDouble();
+            inputs.motorSupplyCurrent = motorSupplyCurrent.getValueAsDouble();
+            // inputs.motorTemperature = motorTemperature.getValueAsDouble();
+            inputs.motorReferenceSlope = motorReferenceSlope.getValueAsDouble();
+        }
+
+    @Override
+    public void setPosition(double position) 
+    {
+        if (GroundIntakeConstants.kUseMotionMagic) 
+        {
+            motor.setControl(motionMagicRequest.withPosition(position));
+        } 
+        else 
+        {
+            motor.setControl(positionRequest.withPosition(position));
+        }
+    }
+
+    @Override
+    public void setVoltage(double voltage) 
+    {
+        motor.setVoltage(voltage);
+    }
+
+    @Override
+    public void off() 
+    {
+        motor.setControl(new NeutralOut());
+    }
+
+    @Override
+    public void zero() 
+    {
+        motor.setPosition(0);
+    }
+
+    @Override
+    public TalonFX getMotor() 
+    {
+        return motor;
+    }
+
+    @Override
+    public VoltageOut getVoltageRequest() 
+    {
+        return voltageReq;
+    }
+
+    @Override
+    public CANcoder getEncoder() {
+        return cancoder;
+  }
+}
