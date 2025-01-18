055f8490f909b6efe57f2ab45c0067af13e6d914
ThatXliner <bryan.hu.2020@gmail.com>
2025-01-18 08:09:37 -0800

    Controller bindings

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 FRC 3256
 // https://github.com/Team3256
 //
-// Use of this source code is governed by a 
+// Use of this source code is governed by a
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
@@ -9,6 +9,7 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -17,6 +18,7 @@ import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.endeffector.EndEffector;
+import frc.robot.utils.MappedXboxController;
 import java.util.HashMap;
 import java.util.Map;
 import org.littletonrobotics.junction.Logger;
@@ -49,7 +51,7 @@ public class Superstructure {
 
   private final Gamepiece[] endeffectorStates = {Gamepiece.EMPTY, Gamepiece.EMPTY};
   //  private ScoringLocations selectedLocation = null;
-  private final int selectedLevel = 0;
+  private int selectedLevel = 0;
   private Gamepiece selectedGamePiece = Gamepiece.EMPTY;
   private final Trigger hasCoral =
       new Trigger(
@@ -61,6 +63,7 @@ public class Superstructure {
   private final Elevator elevator;
   private final Arm arm;
   private final EndEffector endeffector;
+  private final MappedXboxController controller;
 
   // TODO: replace with beambreak
 
@@ -69,7 +72,9 @@ public class Superstructure {
       Arm arm,
       EndEffector endeffector,
       boolean frontHasPreload,
-      boolean backHasPreload) {
+      boolean backHasPreload,
+      MappedXboxController operatorController) {
+    this.controller = operatorController;
     this.elevator = elevator;
     this.arm = arm;
     this.endeffector = endeffector;
@@ -84,6 +89,13 @@ public class Superstructure {
     for (StructureState state : StructureState.values()) {
       stateTriggers.put(state, new Trigger(() -> this.state == state));
     }
+
+    configBeamBreaks();
+    configStateTransitions();
+    configButtonBindings();
+  }
+
+  private void configBeamBreaks() {
     this.endeffector
         .leftBeamBreak
         .onTrue(
@@ -108,7 +120,83 @@ public class Superstructure {
                 () -> {
                   this.endeffectorStates[1] = Gamepiece.EMPTY;
                 }));
-    configStateTransitions();
+  }
+
+  private void configButtonBindings() {
+    // TODO: button bindings for set state to reef, outtake (or outtake and reef), etc
+    // make controller shake if tried to transition to invalid state, but still do it anyway
+    // or override button
+    controller
+        .a("Set level to 1")
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  selectedLevel = 0;
+                  setState(StructureState.REEF);
+                }));
+    controller
+        .b("Set level to 2")
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  selectedLevel = 2;
+                  setState(StructureState.REEF);
+                }));
+    controller
+        .x("Set level to 3")
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  selectedLevel = 3;
+                  setState(StructureState.REEF);
+                }));
+    controller
+        .y("Set level to 4")
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  selectedLevel = 4;
+                  setState(StructureState.REEF);
+                }));
+    controller
+        .leftBumper("Source intake coral")
+        .onTrue(Commands.runOnce(() -> setState(StructureState.SOURCE)));
+    // Perhaps override button to prevent invalid state transitions
+    controller
+        .leftTrigger("Score Coral")
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  if (!stateTriggers.get(StructureState.REEF).getAsBoolean()) {
+                    DriverStation.reportWarning("Invalid state transition to SCORE_CORAL", false);
+                  }
+                  setState(StructureState.SCORE_CORAL);
+                }));
+    controller
+        .rightBumper("Dealgae")
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  if (!stateTriggers.get(StructureState.REEF).getAsBoolean()) {
+                    DriverStation.reportWarning("Invalid state transition to SCORE_CORAL", false);
+                  }
+                  setState(StructureState.DEALGAE);
+                }));
+    controller
+        .rightTrigger("Processor")
+        .onTrue(
+            Commands.runOnce(
+                () -> {
+                  //                              if
+                  // (!stateTriggers.get(StructureState.REEF).getAsBoolean()) {
+                  //                                DriverStation.reportWarning("Invalid state
+                  // transition to SCORE_CORAL", false);
+                  //                              }
+                  //                              setState(StructureState.DEALGAE);
+                }));
+
+    // TODO: manual overrides for elevator and arm (joysticks)
+    // todo: use d-pad as override state transition button
   }
 
   private Command prepareFor(Gamepiece gamepiece) {
@@ -121,8 +209,6 @@ public class Superstructure {
   // TODO: print warnings on invalid state transitions (or just verify and stop invalid
   // transitions)
   public void configStateTransitions() {
-    // TODO: button bindings for set state to reef, outtake (or outtake and reef), etc
-    // make controller shake if tried to transition to invalid state, but still do it anyway
 
     // XXX: dealgae may need diff states
     // which means we may need to select different height dep on selected scoring type
