3f01ad220c95474f09e3fc368a4ed4c1c8112823
Grace Xu <71201761+gxu0904@users.noreply.github.com>
2025-01-25 12:43:02 -0800

    Update Climb Subsytem
    - changed commands
    - added logging

--- src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -7,6 +7,14 @@
 
 package frc.robot.subsystems.climb;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public class ClimbConstants {
   public static final int kClimbMotorID = 0;
   public static final double kMaxCurrent = 30;
@@ -15,4 +23,21 @@ public class ClimbConstants {
   public static final double kLowVoltage = 0.1;
   public static final double kDegreesPerMotorRotation = 360;
   public static final double kMotorTicksPerRotation = 2048;
+  public static final double kUpdateFrequency = 0;
+  public static final boolean kUseMotionMagic = false;
+  public static final TalonFXConfiguration motorConfigs =
+      new TalonFXConfiguration()
+          .withSlot0(new Slot0Configs().withKS(0).withKV(0).withKP(0).withKI(0).withKD(0))
+          .withMotorOutput(
+              new MotorOutputConfigs()
+                  .withNeutralMode(NeutralModeValue.Brake)
+                  .withInverted(InvertedValue.Clockwise_Positive))
+          .withMotionMagic(
+              new MotionMagicConfigs()
+                  .withMotionMagicAcceleration(400)
+                  .withMotionMagicCruiseVelocity(100))
+          .withCurrentLimits(
+              new CurrentLimitsConfigs()
+                  .withStatorCurrentLimitEnable(true)
+                  .withStatorCurrentLimit(80));
 }
