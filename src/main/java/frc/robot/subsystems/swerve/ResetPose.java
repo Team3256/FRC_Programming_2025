60ed832dacc2e531885cb8f5b05b42c23917e2e9
samuel <116925676+memori034@users.noreply.github.com>
2025-02-07 15:18:24 -0800

    spotless

--- src/main/java/frc/robot/subsystems/swerve/ResetPose.java
+++ src/main/java/frc/robot/subsystems/swerve/ResetPose.java
@@ -1,21 +1,24 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.subsystems.swerve;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.RobotContainer;
 
 public class ResetPose extends InstantCommand {
-    private CommandSwerveDrivetrain swerveSubsystem;
+  private CommandSwerveDrivetrain swerveSubsystem;
 
-    public ResetPose(
-            CommandSwerveDrivetrain swerve) {
-        this.swerveSubsystem = swerve;
-        addRequirements(swerveSubsystem);
-    }
+  public ResetPose(CommandSwerveDrivetrain swerve) {
+    this.swerveSubsystem = swerve;
+    addRequirements(swerveSubsystem);
+  }
 
-    @Override
-    public void execute() {
-        this.swerveSubsystem.resetPose(swerveSubsystem.getCurrentPose());
-    }
+  @Override
+  public void execute() {
+    this.swerveSubsystem.resetPose(swerveSubsystem.getCurrentPose());
+  }
 }
