48bac676219ea91eb0da5eb94a1c6ac7fe6e4c44
ThatXliner <bryan.hu.2020@gmail.com>
2025-02-15 11:32:29 -0800

    :loud_sound: Log serial number

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 FRC 3256
 // https://github.com/Team3256
 //
-// Use of this source code is governed by a 
+// Use of this source code is governed by a
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
@@ -124,6 +124,7 @@ public class Robot extends LoggedRobot {
     Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
     Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
     Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    Logger.recordMetadata("SerialNumber", RobotController.getSerialNumber());
 
     // Start logging! No more data receivers, replay sources, or metadata values may
     // be added.
