03bce7a0e9ddedae3150e49255c791c7024367f2
Josh <102478723+realjoshuau@users.noreply.github.com>
2025-02-23 14:22:28 -0800

    fixed auto align

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,11 +13,15 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.units.measure.Distance;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -93,15 +97,14 @@ public final class Constants {
     }
   }
 
-  public static final FieldConstants.BranchHeights branchHeights =
-      FieldConstants.BranchHeights.PRACTICE_FIELD;
+  public static final FieldConstants.BranchHeights branchHeights = FieldConstants.BranchHeights.PRACTICE_FIELD;
 
   /*
    * Robot Constants.
    */
 
   public static class RobotConstants {
-    public static final int bumperLength = 22; // should be right?
-    public static final int bumperWidth = 22;
+    public static final double bumperLength = 0.1; // should be right?
+    public static final double bumperWidth = 22;
   }
 }
