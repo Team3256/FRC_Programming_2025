5b3139b8e1658347efbc1e455cd3e81a131c3354
Josh <102478723+realjoshuau@users.noreply.github.com>
2025-02-23 14:38:22 -0800

    spotless

--- src/main/java/frc/robot/utils/autoaim/CoralTargets.java
+++ src/main/java/frc/robot/utils/autoaim/CoralTargets.java
@@ -56,12 +56,13 @@ public enum CoralTargets {
     this.leftHanded = leftHanded;
   }
 
-  private static final List<Pose2d> transformedPoses = Arrays.stream(values())
-      .map(
-          (CoralTargets targets) -> {
-            return CoralTargets.getRobotTargetLocation(targets.location);
-          })
-      .toList();
+  private static final List<Pose2d> transformedPoses =
+      Arrays.stream(values())
+          .map(
+              (CoralTargets targets) -> {
+                return CoralTargets.getRobotTargetLocation(targets.location);
+              })
+          .toList();
 
   public static Pose2d getRobotTargetLocation(Pose2d original) {
     // return original.transformBy();
@@ -105,9 +106,10 @@ public enum CoralTargets {
         .filter((target) -> target.leftHanded == leftHanded)
         .min(
             Comparator.comparingDouble(
-                (CoralTargets target) -> pose.getTranslation()
-                    .getDistance(
-                        CoralTargets.getRobotTargetLocation(target.location).getTranslation())))
+                (CoralTargets target) ->
+                    pose.getTranslation()
+                        .getDistance(
+                            CoralTargets.getRobotTargetLocation(target.location).getTranslation())))
         .get();
   }
 }
