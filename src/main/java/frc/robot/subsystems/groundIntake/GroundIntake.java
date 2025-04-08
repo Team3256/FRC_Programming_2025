ac218d638f2a074b4360e89764203066a01fb87f
Josh <102478723+realjoshuau@users.noreply.github.com>
2025-04-07 17:19:26 -0700

    ground algae

--- src/main/java/frc/robot/subsystems/groundIntake/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/groundIntake/GroundIntake.java
@@ -19,53 +19,45 @@ public class GroundIntake extends DisableSubsystem {
     public GroundIntake(boolean enabled, GroundIntakeIO groundIntakeIO) {
         super(enabled);
         this.groundIntakeIO = groundIntakeIO;
-        m_sysIdRoutine =
-        new SysIdRoutine(
-            new SysIdRoutine.Config(
-                Volts.of(0.2).per(Seconds), // Use default ramp rate (1 V/s)
-                Volts.of(6), // Reduce dynamic step voltage to 4 to prevent brownout
-                null, // Use default timeout (10 s)
-                // Log state with Phoenix SignalLogger class
-                (state) -> SignalLogger.writeString("state", state.toString())),
-            new SysIdRoutine.Mechanism(
-                (volts) ->
-                    groundIntakeIO
-                        .getMotor()
-                        .setControl(groundIntakeIO.getVoltageRequest().withOutput(volts.in(Volts))),
-                null,
-                this));
+        m_sysIdRoutine = new SysIdRoutine(
+                new SysIdRoutine.Config(
+                        Volts.of(0.2).per(Seconds), // Use default ramp rate (1 V/s)
+                        Volts.of(6), // Reduce dynamic step voltage to 4 to prevent brownout
+                        null, // Use default timeout (10 s)
+                        // Log state with Phoenix SignalLogger class
+                        (state) -> SignalLogger.writeString("state", state.toString())),
+                new SysIdRoutine.Mechanism(
+                        (volts) -> groundIntakeIO
+                                .getMotor()
+                                .setControl(groundIntakeIO.getVoltageRequest().withOutput(volts.in(Volts))),
+                        null,
+                        this));
     }
 
     @Override
-    public void periodic() 
-    {
+    public void periodic() {
         super.periodic();
         groundIntakeIO.updateInputs(groundIntakeIOAutoLogged);
         Logger.processInputs(this.getClass().getSimpleName(), groundIntakeIOAutoLogged);
     }
 
-    public Command setPosition(double position) 
-    {
-        return this.run(() -> groundIntakeIO.setPosition(position * GroundIntakeConstants.SimulationConstants.kGearRatio));
+    public Command setPosition(double position) {
+        return this.run(() -> groundIntakeIO.setPosition(position * 0.2819892));
     }
 
-    public Command off() 
-    {
+    public Command off() {
         return this.runOnce(groundIntakeIO::off);
     }
 
-    public Command zero() 
-    {
+    public Command zero() {
         return this.runOnce(groundIntakeIO::zero);
     }
 
     public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
         return m_sysIdRoutine.quasistatic(direction);
-      }
-    
-      public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
         return m_sysIdRoutine.dynamic(direction);
-      }
+    }
 }
-
-
